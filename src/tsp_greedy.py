75929babdf46e44921cf5ec2096ce9fc4e5193e7
AparnaKale <aparna.kale19@gmail.com>
2018-11-19 21:47:19 -0800

    Helper update

--- src/tsp_greedy.py
+++ src/tsp_greedy.py
@@ -15,65 +15,12 @@ Created on Fri Nov 16 21:15:57 2018
 #  load libraries
 # =============================================================================
 import numpy as np
-import pandas as pd
 import time
 import math
 import random
 import matplotlib.pyplot as plot
+from tsp_helper import plotGraph,loadFile,plot_tsp,getDistance
 
-
-# =============================================================================
-# Plot the path taken by greedy algorithm    
-# =============================================================================
-def framec( solution, nc ):
-    saveme = 0
-    cities = np.array( solution )
-    plot.axis( [-100,1700,-100,1200] )
-    plot.plot(*zip(*cities))
-    plot.title('{} Cities, Greedy Algorithm'.format(nc))
-    plot.show()
-    plot.savefig( ("%05d" % saveme)+'.png')
-    plot.clf()
-
-    
-# =============================================================================
-# load file and parse it
-# =============================================================================
-def loadFile(filename):
-    cities = []
-    with open(filename) as f:
-        lines = f.readlines()
-        start_to_read = False
-        for line in lines:
-            if 'DIMENSION' in line:
-                n_cities = line.split(':')[1]
-                continue
-            if 'EOF' in line:
-                break
-            if 'NODE_COORD_SECTION' in line:
-                start_to_read = True
-                continue
-            if start_to_read:
-                x = line.split(' ')[1]
-                y = line.split(' ')[2]
-                cities.append((float(x),float(y)))
-        return cities
-
-    
-# =============================================================================
-#  Show all the cities    
-# =============================================================================
-def plot_tsp(cities):
-    plot.scatter(*zip(*cities))
-
-    
-# =============================================================================
-#  Find Euclidean Distance 
-# =============================================================================
-def getDistance(currentCity, nextCity):
-    return math.sqrt(np.square(currentCity[0] - nextCity[0]) + np.square(currentCity[1]-nextCity[1]))  
-
-  
 # =============================================================================
 # Traveling Salesman using greedy approach     
 # =============================================================================
@@ -81,7 +28,7 @@ def greedy(cities):
     visited = []        # Initialize
     routeDistance = 0
     mustVisit = cities  # Select a city randomly and set it as home city
-    home = random.choice(cities)
+    home = cities[0]
     visited.append(home)# Maintains the list of cities visited so far
     cities.remove(home)
     currentCity = home
@@ -110,7 +57,7 @@ def main():
     print('----Traveling Salesman Problem by Greedy Method----')
     print('Total distance travelled by Salesman')
     print(tsp_path)
-    framec(all,100)
+    plotGraph(all,100)
 
 if __name__ == '__main__':
     main()
--- src/tsp_helper.py
+++ src/tsp_helper.py
@@ -1,1 +1,55 @@
 #This file contains the helper methods such as plot the graphs, parsing file and finding the distances.
+
+import numpy as np
+import math
+import matplotlib.pyplot as plot
+
+#
+# =============================================================================
+# Plot the path taken by greedy algorithm
+# =============================================================================
+def plotGraph( solution, nc ):
+    saveme = 0
+    cities = np.array( solution )
+    plot.axis( [-100,1700,-100,1200] )
+    plot.plot(*zip(*cities))
+    plot.title('{} Cities, Greedy Algorithm'.format(nc))
+    plot.show()
+    plot.savefig( ("%05d" % saveme)+'.png')
+    plot.clf()
+
+# =============================================================================
+# load file and parse it
+# =============================================================================
+def loadFile(filename):
+    cities = []
+    with open(filename) as f:
+        lines = f.readlines()
+        start_to_read = False
+        for line in lines:
+            if 'DIMENSION' in line:
+                n_cities = line.split(':')[1]
+                continue
+            if 'EOF' in line:
+                break
+            if 'NODE_COORD_SECTION' in line:
+                start_to_read = True
+                continue
+            if start_to_read:
+                x = line.split(' ')[1]
+                y = line.split(' ')[2]
+                cities.append((float(x),float(y)))
+        return cities
+
+
+# =============================================================================
+#  Show all the cities
+# =============================================================================
+def plot_tsp(cities):
+    plot.scatter(*zip(*cities))
+
+# =============================================================================
+#  Find Euclidean Distance
+# =============================================================================
+def getDistance(currentCity, nextCity):
+    return math.sqrt(np.square(currentCity[0] - nextCity[0]) + np.square(currentCity[1]-nextCity[1]))
